3af70608205bbe2e36257ef37daee930cb14ea72
Alex Cruz <mazdarider23@gmail.com>
2015-12-28 13:42:21 -0500

    Power Menu animations [1/2]
    Inspired by what @maxwen did with the power menu back in 5.1.1, I wanted to make
    this optional and add an option for the top
    
    - Slide in from the bottom
    - Slide in from the top
    - Set to the default AOSP animation
    
    Conflicts:
    	core/res/res/values/tesla_styles.xml
    	core/res/res/values/tesla_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4754,6 +4754,13 @@ public final class Settings {
         public static final String DASHBOARD_TILEVIEW_DIVIDERS = "dashboard_tileview_dividers";
 
         /**
+         * Whether to display set of animations when prompting the power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
