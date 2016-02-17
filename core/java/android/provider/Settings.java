570ff24d1b7fdc7fc1c7b4358ed894b5cc185c5c
Ankit Gupta <agupta@cyngn.com>
2016-02-17 15:48:17 -0800

    SettingsProvider: Allow default volume adjust sound to be overlayed.
    Issue-id: YAM-145
    Change-Id: I06394c4fcfca5978b50a4186935ac6bda7b94b16

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4910,6 +4910,12 @@ public final class Settings {
          */
         public static final String QS_TRANSPARENT_SHADE = "qs_transparent_shade";
 
+	/**
+         * Whether the phone volume up / down effect should be played
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
