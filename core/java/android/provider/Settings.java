bde553ce6f67e9b192b99f5dfd62840d5d0e6e5e
rogersb11 <brettrogers11@gmail.com>
2016-03-15 00:43:15 -0400

    Dashboard Text Size 2/2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4832,6 +4832,18 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_SHADOW = "status_bar_custom_header_shadow";
 
         /**
+         * Dashboard title text size
+         * @hide
+         */
+        public static final String SETTINGS_TITLE_TEXT_SIZE = "settings_title_text_size";
+
+        /**
+         * Dashboard category text size
+         * @hide
+         */
+        public static final String SETTINGS_CATEGORY_TEXT_SIZE = "settings_category_text_size";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
