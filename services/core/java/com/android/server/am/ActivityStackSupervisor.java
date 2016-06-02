b7c032fa657cac0900a6b93649a52ff12389cbc4
Hall Liu <hallliu@google.com>
2016-06-02 10:32:08 -0700

    DO NOT MERGE Disable app pinning when emergency call button pressed
    Also disables app pinning when the "return to call" button is pressed
    and brings up the in-call screen when app pinning is stopped if there is
    an existing call.
    
    Combination of ag/1091397 and ag/1085584 adapted for MNC.
    
    Bug: 28558307
    Bug: 28761672
    Change-Id: I82ec4042bff387c845ce571b197a4a86e1dd5ec8

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -3869,7 +3869,9 @@ public final class ActivityStackSupervisor implements DisplayListener {
     }
 
     void showLockTaskToast() {
-        mLockTaskNotify.showToast(mLockTaskModeState);
+        if (mLockTaskNotify != null) {
+            mLockTaskNotify.showToast(mLockTaskModeState);
+        }
     }
 
     void showLockTaskEscapeMessageLocked(TaskRecord task) {
