XXMrHyde/android_frameworks_base

Status bar: Greeting, (1/2):
Ported from Darkkat 5.1:
Status bar: Greeting, (1/2):
https://github.com/XXMrHyde/android_frameworks_base/commit/68f9ac5361efd56929d7fa2d52762c319e560efb

Status bar, Greeting: improvements, (1/2):
https://github.com/XXMrHyde/android_frameworks_base/commit/60fe167a73f421837fa81ae48177bfe9901e5c97

Included:
- Show greeting:
  - Always, (every time the decice gets unlocked from lock screen)
  - Once, , (the first time the decice gets unlocked from lock screen after boot)
  - never
- Custom text
- Duration, (the time between fade in and fade out)
- Show preview
- Text color
- Text color dark mode

Changes:
- Use the default alpha fade in/out animations,
  (on 6.0 there is no notification ticker,
  to be consistent use the same animations the status bar is using, too)
- Add color dark mode

core/java/com/android/internal/util/darkkat/GreetingTextHelper.java
@@ -1,0 +1,43 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.darkkat;
+
+import android.content.Context;
+import android.os.Build;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class GreetingTextHelper {
+
+    public static String getDefaultGreetingText(Context context) {
+
+        final String welcome = context.getResources().getString(
+                com.android.internal.R.string.default_greeting_welcome);
+        Date date = new Date(Build.TIME); 
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+1"));
+        String buildDate = sdf.format(date);
+
+        String defaultGreetingText =
+                welcome + " " + Build.VERSION.RELEASE
+                + " (" + Build.ID + "), " + buildDate;
+
+        return defaultGreetingText;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -104,6 +104,7 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.statusbar.NotificationVisibility;
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.util.darkkat.DeviceUtils;
+import com.android.internal.util.darkkat.GreetingTextHelper;
 
 import com.android.keyguard.CarrierText;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
@@ -448,6 +449,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.SCREEN_BRIGHTNESS_MODE),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_SHOW_GREETING),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_CUSTOM_TEXT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_COLOR),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CARRIER_LABEL_USE_CUSTOM),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -549,6 +562,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 || uri.equals(Settings.System.getUriFor(
                     Settings.System.SCREEN_BRIGHTNESS_MODE))) {
                 updateBrightnessControl();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_SHOW_GREETING))) {
+                updateShowGreeting();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_CUSTOM_TEXT))) {
+                updateGreetingText();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT))) {
+                updateGreetingTimeout();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_COLOR))) {
+                updateGreetingColor();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CARRIER_LABEL_USE_CUSTOM))
                 || uri.equals(Settings.System.getUriFor(
@@ -1152,6 +1177,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction("com.android.settings.SHOW_GREETING_PREVIEW");
         context.registerReceiverAsUser(mBroadcastReceiver, UserHandle.ALL, filter, null, null);
 
         IntentFilter demoFilter = new IntentFilter();
@@ -2018,6 +2044,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void updateSettings() {
         updateBrightnessControl();
+        updateShowGreeting();
+        updateGreetingText();
+        updateGreetingTimeout();
+        updateGreetingColor();
         updateCarrierLabelVisibility();
         updateLockScreenCarrierLabelVisibility();
         updateCarrierLabelColor(false);
@@ -2043,6 +2073,41 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mAutomaticBrightness = mode != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
         mBrightnessControl = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0) == 1;
+    }
+
+    private void updateShowGreeting() {
+        final int showGreeting = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_GREETING_SHOW_GREETING, 1);
+        if (mIconController != null) {
+            mIconController.updateShowGreeting(showGreeting);
+        }
+    }
+
+    private void updateGreetingText() {
+        String greetingText = Settings.System.getString(
+                mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_GREETING_CUSTOM_TEXT);
+
+        if (greetingText == null || greetingText.isEmpty()) {
+            greetingText = GreetingTextHelper.getDefaultGreetingText(mContext);
+        }
+        if (mIconController != null) {
+            mIconController.updateGreetingText(greetingText);
+        }
+    }
+
+    private void updateGreetingTimeout() {
+        final int greetingTimeout = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_GREETING_TIMEOUT, 400);
+        if (mIconController != null) {
+            mIconController.updateGreetingTimeout(greetingTimeout);
+        }
+    }
+
+    private void updateGreetingColor() {
+        if (mIconController != null) {
+            mIconController.updateGreetingColor();
+        }
     }
 
     private void updateCarrierLabelSettings() {
@@ -3410,6 +3475,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
                 mScreenOn = false;
+                if (mIconController != null) {
+                    mIconController.resetHideGreeting();
+                }
                 notifyNavigationBarScreenOn(false);
                 notifyHeadsUpScreenOff();
                 finishBarAnimations();
@@ -3418,6 +3486,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 mScreenOn = true;
                 notifyNavigationBarScreenOn(true);
+            }
+            else if (action.equals("com.android.settings.SHOW_GREETING_PREVIEW")) {
+                if (mIconController != null) {
+                    mIconController.showGreeting(true);
+                }
             }
         }
     };

YELLOWJACKET1212/frameworks_base

Change-Id: I7dae70a5603a65f0fe27211a4256748bcb6ec289

core/java/com/android/internal/util/tesla/GreetingTextHelper.java
@@ -1,0 +1,43 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.tesla;
+
+import android.content.Context;
+import android.os.Build;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class GreetingTextHelper {
+
+    public static String getDefaultGreetingText(Context context) {
+
+        final String welcome = context.getResources().getString(
+                com.android.internal.R.string.default_greeting_welcome);
+        Date date = new Date(Build.TIME);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+1"));
+        String buildDate = sdf.format(date);
+
+        String defaultGreetingText =
+                welcome + " " + Build.VERSION.RELEASE
+                + " (" + Build.ID + "), " + buildDate;
+
+        return defaultGreetingText;
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -114,6 +114,8 @@ import android.widget.TextView;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.statusbar.NotificationVisibility;
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.internal.util.tesla.StatusBarColorHelper;
+import com.android.internal.util.tesla.GreetingTextHelper;
 import com.android.internal.util.du.WeatherControllerImpl;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.KeyguardUpdateMonitor;
@@ -446,6 +448,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_SHOW_GREETING),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_CUSTOM_TEXT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_COLOR),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -461,6 +475,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mBatterySaverWarningColor = mContext.getResources()
                                 .getColor(com.android.internal.R.color.battery_saver_mode_color);
                     }
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_SHOW_GREETING))) {
+                updateShowGreeting();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_CUSTOM_TEXT))) {
+                updateGreetingText();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT))) {
+                updateGreetingTimeout();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_COLOR))) {
+                updateGreetingColor();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.USE_SLIM_RECENTS))) {
                 updateRecents();
@@ -480,6 +506,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mTeslaLogoColor = Settings.System.getIntForUser(resolver,
                     Settings.System.STATUS_BAR_TESLA_LOGO_COLOR, 0xFFFFFFFF, mCurrentUserId);
             showTeslaLogo(mTeslaLogo, mTeslaLogoColor);
+
+            updateShowGreeting();
+            updateGreetingText();
+            updateGreetingTimeout();
+            updateGreetingColor();
+
         }
     }
 
@@ -1118,6 +1150,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED);
+        filter.addAction("com.android.settings.SHOW_GREETING_PREVIEW");
         context.registerReceiverAsUser(mBroadcastReceiver, UserHandle.ALL, filter, null, null);
 
         IntentFilter demoFilter = new IntentFilter();
@@ -2014,6 +2047,41 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     }
                 }
             }
+        }
+    }
+
+    private void updateShowGreeting() {
+        final int showGreeting = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_GREETING_SHOW_GREETING, 1);
+        if (mIconController != null) {
+            mIconController.updateShowGreeting(showGreeting);
+        }
+    }
+
+    private void updateGreetingText() {
+        String greetingText = Settings.System.getString(
+                mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_GREETING_CUSTOM_TEXT);
+
+        if (greetingText == null || greetingText.isEmpty()) {
+            greetingText = GreetingTextHelper.getDefaultGreetingText(mContext);
+        }
+        if (mIconController != null) {
+            mIconController.updateGreetingText(greetingText);
+        }
+    }
+
+    private void updateGreetingTimeout() {
+        final int greetingTimeout = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_GREETING_TIMEOUT, 400);
+        if (mIconController != null) {
+            mIconController.updateGreetingTimeout(greetingTimeout);
+        }
+    }
+
+    private void updateGreetingColor() {
+        if (mIconController != null) {
+            mIconController.updateGreetingColor();
         }
     }
 
@@ -3241,6 +3309,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
             else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+                if (mIconController != null) {
+                    mIconController.resetHideGreeting();
+                }
                 notifyNavigationBarScreenOn(false);
                 notifyHeadsUpScreenOff();
                 finishBarAnimations();
@@ -3276,6 +3347,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             } else if (Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED.equals(action)) {
                 updateMediaMetaData(true);
+            }
+            else if (action.equals("com.android.settings.SHOW_GREETING_PREVIEW")) {
+                if (mIconController != null) {
+                    mIconController.showGreeting(true);
+                }
             }
         }
     };